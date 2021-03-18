f0d86347970f100c0a176198e443b93886bd4fa8
peiyongkang <peiyongkang@zhihu.com>
2021-03-18 18:46:18 +0800

    feat: 简陋版本开发完成

---
+++ pchi.go
@@ -0,0 +1,92 @@
+package pchi
+
+import (
+	"context"
+	"net/http"
+)
+
+type HttpMethodType int
+
+const (
+	Get HttpMethodType = 1 << iota
+	Head
+	Post
+	Put
+	Patch
+	Delete
+	Connect
+	Options
+	Trace
+)
+
+var RouterContextKey = &contextKey{name: "RouterContextKey"}
+
+const AllMethod = Get | Head | Post | Put | Patch | Delete | Connect | Options | Trace
+
+var HttpMethodMap = map[string]HttpMethodType{
+	http.MethodGet:     Get,
+	http.MethodHead:    Head,
+	http.MethodPost:    Post,
+	http.MethodPut:     Put,
+	http.MethodPatch:   Patch,
+	http.MethodDelete:  Delete,
+	http.MethodConnect: Connect,
+	http.MethodOptions: Options,
+	http.MethodTrace:   Trace,
+}
+
+var HttpMethodString = map[HttpMethodType]string{
+	Get:     http.MethodGet,
+	Head:    http.MethodHead,
+	Post:    http.MethodPost,
+	Put:     http.MethodPut,
+	Patch:   http.MethodPatch,
+	Delete:  http.MethodDelete,
+	Connect: http.MethodConnect,
+	Options: http.MethodOptions,
+	Trace:   http.MethodTrace,
+}
+
+type MiddleWare func(next http.Handler) http.Handler
+
+type RouterContext struct {
+	ParamKey   []string
+	ParamValue []string
+}
+
+func (context *RouterContext) Clean() {
+	context.ParamValue = nil
+	context.ParamKey = nil
+}
+
+type contextKey struct {
+	name string
+}
+
+func GetURLParam(context context.Context, key string) string {
+	return GetURLParamByIndex(context, key, 1)
+}
+
+func GetURLParamByIndex(context context.Context, key string, index int) string {
+	routerContext, ok := context.Value(RouterContextKey).(*RouterContext)
+	if !ok {
+		return ""
+	}
+	var keyIndex int
+	for paramIndex, paramKey := range routerContext.ParamKey {
+		if paramKey == key {
+			keyIndex++
+		}
+		if keyIndex == index {
+			return routerContext.ParamValue[paramIndex]
+		}
+	}
+
+	return ""
+}
+
+type HttpRouter interface {
+	http.Handler
+	RouterHandler(pattern string, methodType HttpMethodType, handler http.Handler)
+	Middleware(middleware MiddleWare)
+}
---
+++ tree.go
@@ -0,0 +1,320 @@
+package pchi
+
+import (
+	"fmt"
+	"net/http"
+	regexp2 "regexp"
+	"sort"
+	"strings"
+)
+
+type NodeType int
+
+const (
+	static   NodeType = iota // 静态链接: /hello/check_heath
+	param                    // 带参数的链接： /sku/{type}
+	regexp                   // 正则表达式的链接 /sku/{id:[0-9]+}
+	wildcard                 // 带 * 的链接 /sku/*
+)
+
+type EndPoint struct {
+	Pattern string
+	Handler http.Handler
+}
+
+type nodes []*Node
+
+func (n nodes) Len() int {
+	return len(n)
+}
+
+func (n nodes) Less(i, j int) bool {
+	return n[i].Prefix < n[j].Prefix
+}
+
+func (n nodes) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+type Node struct {
+	NodeType     NodeType
+	Prefix       string
+	Child        [wildcard + 1]nodes
+	EndPoints    map[HttpMethodType]EndPoint
+	ParamKey     string
+	Express      string
+	RegexpMethod *regexp2.Regexp
+}
+
+func (node *Node) InsertNode(pattern string, method HttpMethodType, handler http.Handler) *Node {
+	parent := node
+	search := pattern
+
+	for {
+		var bi, be int
+		var find bool
+		nodeType, es, ed, key, express := ParsePattern(search)
+		if search[0] == '{' || search[0] == '*' {
+			be = ed
+		} else {
+			be = es - 1
+			if es == 0 {
+				be = ed
+			}
+			nodeType = static
+		}
+
+		prefix := search[bi : be+1]
+		child := parent.GetChild(nodeType, prefix, key, express)
+		if child == nil {
+			child = &Node{
+				NodeType: nodeType,
+				Prefix:   prefix,
+			}
+			if nodeType > static {
+				child.ParamKey = key
+				child.Express = express
+			}
+			if child.Express != "" {
+				rm, err := regexp2.Compile(child.Express)
+				if err != nil {
+					panic(fmt.Sprintf("pchi:编译正则表达式失败，express = %s, pattern = %s, method = %s, err = %s", child.Express, pattern, HttpMethodString[method], err.Error()))
+				}
+				child.RegexpMethod = rm
+			}
+			parent.AddChild(child)
+			find = true
+		}
+
+		// { 或者 * 开头，继续查找后面的部分
+		if find || nodeType != static {
+			if be == len(search)-1 {
+				child.AddEndPoint(method, pattern, handler)
+				return child
+			}
+			parent = child
+			search = search[be+1:]
+			continue
+		}
+
+		pi := patternEqLen(child.Prefix, prefix)
+
+		// 1、child.Prefix 为 /ab prefix 为 /abc
+		// 2、child.Prefix 为 /ab prefix 为 /ab
+		if pi == len(child.Prefix) {
+			if es == 0 && child.Prefix == prefix {
+				child.AddEndPoint(method, pattern, handler)
+				return child
+			}
+			parent = child
+			search = search[bi+pi:]
+			continue
+		}
+
+		// 1、child.Prefix 为 /ab，prefix 为 /ac
+		// 2、child.Prefix 为 /ab，prefix 为 /a
+		oldChild := *child
+		child.Clean()
+		child.Prefix = prefix[:pi]
+		oldChild.Prefix = oldChild.Prefix[pi:]
+		child.AddChild(&oldChild)
+
+		if pi < len(prefix) {
+			newChild := &Node{
+				NodeType: static,
+				Prefix:   prefix[pi:],
+			}
+			child.AddChild(newChild)
+			child = newChild
+		}
+		// search 为 static
+		if es == 0 {
+			child.AddEndPoint(method, pattern, handler)
+			return child
+		}
+
+		parent = child
+		search = search[be+1:]
+	}
+}
+
+func (node *Node) FindNode(context *RouterContext, pattern string) *Node {
+	parent := node
+	search := pattern
+
+	for {
+		child := parent.findChild(search)
+		if child == nil {
+			return child
+		}
+		switch child.NodeType {
+		case param, regexp:
+			index := strings.IndexByte(search, '/')
+			if index == -1 {
+				index = len(search)
+			}
+			context.ParamKey = append(context.ParamKey, child.ParamKey)
+			context.ParamValue = append(context.ParamValue, search[:index])
+			if index == len(search) {
+				return child
+			}
+			parent = child
+			search = search[index:]
+		case wildcard:
+			return child
+		case static:
+			if search == child.Prefix {
+				return child
+			}
+			pi := patternEqLen(child.Prefix, search)
+			parent = child
+			search = search[pi:]
+		}
+	}
+}
+
+func (node *Node) FindHandler(context *RouterContext, pattern string, method HttpMethodType) http.Handler {
+	child := node.FindNode(context, pattern)
+	if child == nil {
+		return nil
+	}
+	ep, ok := child.EndPoints[method]
+	if ok {
+		return ep.Handler
+	}
+
+	return nil
+}
+
+func (node *Node) findChild(pattern string) *Node {
+	for _, nodes := range node.Child {
+		for _, node := range nodes {
+			switch node.NodeType {
+			case static:
+				if pattern[0] == node.Prefix[0] {
+					return node
+				}
+			case param, wildcard:
+				return node
+			case regexp:
+				index := strings.IndexByte(pattern, '/')
+				if index == -1 {
+					index = len(pattern)
+				}
+				if node.RegexpMethod.MatchString(pattern[:index]) {
+					return node
+				}
+
+			}
+		}
+	}
+	return nil
+}
+
+func (node *Node) AddChild(child *Node) {
+	node.Child[child.NodeType] = append(node.Child[child.NodeType], child)
+	sort.Sort(node.Child[child.NodeType])
+}
+
+func (node *Node) GetChild(nodeType NodeType, pattern, key, reg string) *Node {
+	for _, child := range node.Child[nodeType] {
+		switch nodeType {
+		case static:
+			if child.Prefix[0] == pattern[0] {
+				return child
+			}
+		case param:
+			if child.ParamKey == key {
+				return child
+			}
+		case regexp:
+			if child.ParamKey == key && child.Express == reg {
+				return child
+			}
+		case wildcard:
+			return child
+		}
+	}
+
+	return nil
+}
+
+func ParsePattern(pattern string) (NodeType, int, int, string, string) {
+	pl := len(pattern)
+	expressStart := strings.IndexByte(pattern, '{')
+	wildcardStart := strings.IndexByte(pattern, '*')
+
+	if wildcardStart >= 0 {
+		if wildcardStart != len(pattern)-1 {
+			panic(fmt.Sprintf("pchi: 通配符 * 必须出现在链接末尾, pattern = %s", pattern))
+		}
+		return wildcard, wildcardStart, wildcardStart + 1, "", ""
+	}
+
+	if expressStart >= 0 {
+		nodeType := param
+		var key, reg string
+		expressCount := 1
+		var expressEnd int
+		for i := expressStart + 1; i < pl; i++ {
+			if pattern[i] == '{' {
+				expressCount++
+			} else if pattern[i] == '}' {
+				expressCount--
+			}
+			if expressCount == 0 {
+				expressEnd = i
+				break
+			}
+		}
+		key = pattern[expressStart+1 : expressEnd]
+		if params := strings.Split(key, ":"); len(params) > 1 {
+			key = params[0]
+			reg = params[1]
+			// 强制全部匹配
+			if reg[0] != '^' {
+				reg = "^" + reg
+			}
+			if reg[len(reg)-1] != '$' {
+				reg = reg + "$"
+			}
+			nodeType = regexp
+		}
+		return nodeType, expressStart, expressEnd, key, reg
+	}
+
+	return static, 0, pl - 1, "", ""
+
+}
+
+func (node *Node) Clean() {
+	node.Prefix = ""
+	node.EndPoints = nil
+	node.Express = ""
+	node.ParamKey = ""
+	node.Child = [4]nodes{}
+}
+
+func (node *Node) AddEndPoint(method HttpMethodType, pattern string, handler http.Handler) {
+	if node.EndPoints == nil {
+		node.EndPoints = map[HttpMethodType]EndPoint{method: {Pattern: pattern, Handler: handler}}
+		return
+	}
+	_, ok := node.EndPoints[method]
+	if ok {
+		panic(fmt.Sprintf("pchi: 已经存在方法 %s 对应的 handler 了，pattern = %s", HttpMethodString[method], pattern))
+	}
+	node.EndPoints[method] = EndPoint{Pattern: pattern, Handler: handler}
+}
+
+func patternEqLen(s1, s2 string) int {
+	for i := 0; i < len(s1); i++ {
+		if i == len(s2) {
+			return i
+		}
+		if s1[i] != s2[i] {
+			return i
+		}
+	}
+	return len(s1)
+}
